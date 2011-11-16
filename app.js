d59d696949f75e750f0a7e8bac4f419afd0ffa1a
Ankit Shankar <ankitshankar@gmail.com>
2011-11-16 02:38:46 -0500

    Hack!

---
+++ app.js
@@ -0,0 +1,45 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+   , routes = require('./routes')
+       , io = require('socket.io');
+
+var app = module.exports = express.createServer();
+
+io = io.listen(app);
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+
+io.sockets.on('connection', function(socket) {
+    socket.on('path', function(data) {
+        io.sockets.emit('path', data);
+    });
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
